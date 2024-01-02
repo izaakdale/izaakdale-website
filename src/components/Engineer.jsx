73d471041261bd9ee7aeb634b405de44d0d8609f
Izaak Dale <izaakdaledev@gmail.com>
2024-01-02 15:19:02 -0800

    few tailwind changes

--- src/components/Engineer.jsx
+++ src/components/Engineer.jsx
@@ -46,7 +46,7 @@ export default function Engineer() {
       <div id='txt-2' className='absolute top-[5%] right-[8%]'>
         5+ years experience
       </div>
-      <div id='txt-3' className='absolute top-[8%] left-[8%]'>
+      <div id='txt-3' className='absolute top-[8%] left-[25%]'>
         Backend
       </div>
       <div id='txt-4' className=' absolute top-[10%] left-[48%] text-6xl'>
@@ -61,7 +61,7 @@ export default function Engineer() {
       <div id='txt-7' className='absolute top-[14%] left-[16%]'>
         Cloud
       </div>
-      <div id='txt-8' className='absolute top-[19%] left-[10%] text-5xl'>
+      <div id='txt-8' className='absolute top-[21%] left-[24%] text-5xl'>
         <SiKubernetes />
       </div>
       <div id='txt-9' className='absolute top-[25%] left-[38%] text-5xl'>
@@ -70,10 +70,10 @@ export default function Engineer() {
       <div id='txt-10' className='absolute top-[26%] right-[21%] text-5xl'>
         <FaDocker />
       </div>
-      <div id='txt-11' className='absolute top-[31%] left-[10%] text-5xl'>
+      <div id='txt-11' className='absolute top-[31%] left-[19%] text-5xl'>
         <FaRust />
       </div>
-      <div id='txt-12' className='absolute top-[34%] left-[45%]'>
+      <div id='txt-12' className='absolute top-[34%] left-[55%]'>
         Cybersecutiry
       </div>
     </div>
